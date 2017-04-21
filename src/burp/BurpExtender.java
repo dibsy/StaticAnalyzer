7d2f58f86361daa7556043dd3d5d32ccd6f1f6fa
Dibyendu <dibyendu.coder@gmail.com>
2017-04-22 00:08:01 +0530

    First Commit

---
+++ src/burp/BurpExtender.java
@@ -0,0 +1,229 @@
+package burp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class BurpExtender implements IBurpExtender, IScannerCheck
+{
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+
+    // test / grep strings
+    private static final byte[][]GREP_STRING_ARRAY = {"eval()".getBytes(),"git".getBytes()};// I need to modify this line...
+    private static final byte[] INJ_TEST = "|".getBytes();
+    private static final byte[] INJ_ERROR = "Unexpected pipe".getBytes();
+    
+    //
+    // implement IBurpExtender
+    //
+    
+    @Override
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
+    {
+        // keep a reference to our callbacks object
+        this.callbacks = callbacks;
+        
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
+        
+        // set our extension name
+        callbacks.setExtensionName("Static Analyis Check");
+        
+        
+        // register ourselves as a custom scanner check
+        callbacks.registerScannerCheck(this);
+    }
+    
+    // helper method to search a response for occurrences of a literal match string
+    // and return a list of start/end offsets
+    private List<int[]> getMatches(byte[] response, byte[] match)
+    {
+        List<int[]> matches = new ArrayList<int[]>();
+
+        int start = 0;
+        while (start < response.length)
+        {
+            start = helpers.indexOf(response, match, true, start, response.length);
+            if (start == -1)
+                break;
+            matches.add(new int[] { start, start + match.length });
+            start += match.length;
+        }
+        
+        return matches;
+    }
+
+    //
+    // implement IScannerCheck
+    //
+    
+    @Override
+    public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse)
+    {
+    	 // look for matches of our passive check grep string
+    	//byte[]GREP_STRING="eval(".getBytes();
+    	ArrayList<byte[]>vectors=new ArrayList<byte[]>();
+    	
+    	try {
+			Scanner sc=new Scanner(new File("vectors.txt"));
+			while(sc.hasNextLine()){
+				vectors.add(sc.nextLine().getBytes());
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+ //   	vectors.add("eval(".getBytes());
+ //   	vectors.add("git".getBytes());
+    	
+    	
+    	List<IScanIssue> issues = new ArrayList<>();
+    	Iterator<byte[]> iterator = vectors.iterator();
+    	while (iterator.hasNext()) {
+    		byte[]GREP_STRING=iterator.next();
+    	
+    		List<int[]> matches = getMatches(baseRequestResponse.getResponse(), GREP_STRING);
+    		if (matches.size() > 0)
+    		{
+            // report the issue
+            
+    				issues.add(new CustomScanIssue(
+                    baseRequestResponse.getHttpService(),
+                    helpers.analyzeRequest(baseRequestResponse).getUrl(), 
+                    new IHttpRequestResponse[] { callbacks.applyMarkers(baseRequestResponse, null, matches) }, 
+                    "Info Leakage",
+                    "The response contains the string: " + helpers.bytesToString(GREP_STRING),
+                    "Information"));
+    		}
+        }
+    	if(issues.size()>0)
+    		return issues;
+    	else
+    		return null;
+    }
+
+    @Override
+    public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue)
+    {
+        // This method is called when multiple issues are reported for the same URL 
+        // path by the same extension-provided check. The value we return from this 
+        // method determines how/whether Burp consolidates the multiple issues
+        // to prevent duplication
+        //
+        // Since the issue name is sufficient to identify our issues as different,
+        // if both issues have the same name, only report the existing issue
+        // otherwise report both issues
+        if (existingIssue.getIssueName().equals(newIssue.getIssueName()))
+            return -1;
+        else return 0;
+    }
+
+    @Override
+    public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse, IScannerInsertionPoint insertionPoint)
+    {
+    	return null;
+    }
+}
+
+//
+// class implementing IScanIssue to hold our custom scan issue details
+//
+class CustomScanIssue implements IScanIssue
+{
+    private IHttpService httpService;
+    private URL url;
+    private IHttpRequestResponse[] httpMessages;
+    private String name;
+    private String detail;
+    private String severity;
+
+    public CustomScanIssue(
+            IHttpService httpService,
+            URL url, 
+            IHttpRequestResponse[] httpMessages, 
+            String name,
+            String detail,
+            String severity)
+    {
+        this.httpService = httpService;
+        this.url = url;
+        this.httpMessages = httpMessages;
+        this.name = name;
+        this.detail = detail;
+        this.severity = severity;
+    }
+    
+    @Override
+    public URL getUrl()
+    {
+        return url;
+    }
+
+    @Override
+    public String getIssueName()
+    {
+        return name;
+    }
+
+    @Override
+    public int getIssueType()
+    {
+        return 0;
+    }
+
+    @Override
+    public String getSeverity()
+    {
+        return severity;
+    }
+
+    @Override
+    public String getConfidence()
+    {
+        return "Certain";
+    }
+
+    @Override
+    public String getIssueBackground()
+    {
+        return null;
+    }
+
+    @Override
+    public String getRemediationBackground()
+    {
+        return null;
+    }
+
+    @Override
+    public String getIssueDetail()
+    {
+        return detail;
+    }
+
+    @Override
+    public String getRemediationDetail()
+    {
+        return null;
+    }
+
+    @Override
+    public IHttpRequestResponse[] getHttpMessages()
+    {
+        return httpMessages;
+    }
+
+    @Override
+    public IHttpService getHttpService()
+    {
+        return httpService;
+    }
+    
+}
